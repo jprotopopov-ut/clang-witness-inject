f9ec90721a0a88fb2882473fa1f94785854c0d99
Jevgenijs Protopopovs <jevgenijs.protopopovs@ut.ee>
2026-01-23 12:07:50 +0200

    Extend the fuzz script to compile the program in-place

--- run_svcomp_fuzz.py
+++ run_svcomp_fuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import os
 import time
 import pathlib
 import argparse
@@ -7,13 +8,22 @@ import tempfile
 import shutil
 import zipfile
 import logging
+import subprocess
+import shlex
 import aflpp_ctrl
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+FUZZ_HARNESS_DIR = SCRIPT_DIR / 'fuzz_harness'
+EXAMPLES_DIR = SCRIPT_DIR / 'examples'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog=sys.argv[0], description='AFL++ driver for SV-COMP benchmarks')
     parser.add_argument('--aflpp', type=str, required=True, help='AFL++ installation directory')
     parser.add_argument('--state', type=str, required=False, help='State archive')
     parser.add_argument('--resume', default=False, action='store_true', help='Output directory')
+    parser.add_argument('--compile', default=False, action='store_true', help='Compile benchmark program')
+    parser.add_argument('--compile-cflags', type=str, default='', help='Compilation CFLAGS')
+    parser.add_argument('--compile-ldflags', type=str, default='', help='Compilation LDFLAGS')
     parser.add_argument('--quiet', default=False, action='store_true', help='Suppress normal AFL++ output')
     parser.add_argument('--input-seed', type=str, required=False, help='Initial input seed for fuzzing')
     parser.add_argument('--harness-timeout', type=int, default=5000, help='Fuzz harness timeout in microseconds')
@@ -35,6 +45,8 @@ if __name__ == '__main__':
         misc_dir_path = work_dir_path / 'misc'
         program_filepath = misc_dir_path / 'program'
 
+        aflpp_dir = pathlib.Path(args.aflpp).resolve()
+
         logger.info('Workdir %s', work_dir_path)
 
         if args.resume and args.state:
@@ -43,6 +55,7 @@ if __name__ == '__main__':
                 logger.warning('Resuming fuzzing: %s is ignored in favor from archived version from %s', args.program, args.state)
             with zipfile.ZipFile(args.state, 'r') as archive:
                 archive.extractall(work_dir_path)
+            os.chmod(program_filepath, 500)
         else:
             if not args.program:
                 logger.critical('Missing program command line argument')
@@ -50,7 +63,24 @@ if __name__ == '__main__':
             input_dir_path.mkdir()
             output_dir_path.mkdir()
             misc_dir_path.mkdir()
-            shutil.copy(args.program, program_filepath)
+
+            if args.compile:
+                logger.info('Compiling %s', args.program)
+                subprocess.check_call([
+                    aflpp_dir / 'bin' / 'afl-clang-lto',
+                    '-w', '-fPIC', '-DFUZZ_HARNESS_RAND_STDIN=1',
+                    '-include', str(FUZZ_HARNESS_DIR / 'fuzz_harness.h'),
+                    '-include', str(EXAMPLES_DIR / 'assert.h'),
+                    *shlex.split(args.compile_cflags),
+                    args.program,
+                    str(FUZZ_HARNESS_DIR / 'fuzz_harness.c'),
+                    str(FUZZ_HARNESS_DIR / 'fuzz_harness.s'),
+                    '-o', str(program_filepath),
+                    *shlex.split(args.compile_ldflags),
+                    '-Wl,-e,__fuzz_harness_start'
+                ])
+            else:
+                shutil.copy(args.program, program_filepath)
 
         try:
             with open(input_dir_path / 'input1', 'wb') as sample_input:
@@ -59,7 +89,7 @@ if __name__ == '__main__':
                 sample_input.write(input_seed)
             
             config = aflpp_ctrl.AFLppFuzzConfig(
-                aflpp_dir=pathlib.Path(args.aflpp),
+                aflpp_dir=aflpp_dir,
                 skip_cpufreq=True,
                 quiet=args.quiet
             )
